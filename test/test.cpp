4765c9b0ee7b8712a015ad04e0e27e9a026fb557
Paul D'Angio <pcdangio@gmail.com>
2020-08-24 12:11:27 -0400

    updated test to include sin experiment

--- test/test.cpp
+++ test/test.cpp
@@ -4,18 +4,19 @@
 
 double objective_function(const Eigen::VectorXd& variables)
 {
-    return std::pow(variables(0) - 4.75, 2.0) + std::pow(variables(1) - 2, 2.0);
+    //return std::sin(1 + variables(0));// - 0.00001/(variables(0) - 6.0) - 0.00001/(2.0-variables(0));
+    return std::pow(variables(0) + 4.75, 2.0) + std::pow(variables(1) - 2, 2.0);
 }
 void objective_gradient(const Eigen::VectorXd& operating_point, Eigen::VectorXd& gradient)
 {
-    gradient(0) = 2*(operating_point(0) - 4.75);
-    gradient(1) = 2*(operating_point(1) - 2);
+    gradient(0) = std::cos(1+operating_point(0));
+    // gradient(0) = 2*(operating_point(0) - 4.75);
+    // gradient(1) = 2*(operating_point(1) - 2);
 }
 
 int32_t main(int32_t argc, char** argv)
 {
     qn_optimizer qno(2, &objective_function);
-    //qno.set_objective_gradient(&objective_gradient);
 
     Eigen::VectorXd variables;
     variables.setZero(2);
